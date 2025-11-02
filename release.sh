6fda1dd11aea1bf358ede6296dafca01af4e95a2
bigshans <26884666+bigshans@users.noreply.github.com>
2025-11-02 18:16:54 +0800

    chore: allow upload when create tag failed

--- release.sh
+++ release.sh
@@ -1,1 +1,4 @@
-gh release create v$1-bore --generate-notes && gh release upload v$1-bore linux-xanmod-bore-cjktty-$1-1-x86_64.pkg.tar.zst linux-xanmod-bore-cjktty-headers-$1-1-x86_64.pkg.tar.zst
+set +e
+
+gh release create v$1-bore --generate-notes
+gh release upload v$1-bore linux-xanmod-bore-cjktty-$1-1-x86_64.pkg.tar.zst linux-xanmod-bore-cjktty-headers-$1-1-x86_64.pkg.tar.zst
